e747b17534b4f5adefa86d052c55aecb1b53db66
rizwan.199811 <rizwan.199811@gmail.com>
2021-08-20 21:54:11 +0500

    changes done

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaskRepository } from './task.respository';
 
 
 @Module({
- 
+ imports:[TypeOrmModule.forFeature([TaskRepository])]
 })
 export class TaskModule {}
